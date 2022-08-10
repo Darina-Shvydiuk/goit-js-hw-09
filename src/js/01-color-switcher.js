3d613859a1728b4ecb859a0c1387c6aedf02ffae
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-08-10 23:51:54 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+const DELAY = 1000;
+let timeoutId = null;
+btnStart.addEventListener('click', onTargetBtnStart);
+btnStop.addEventListener('click', onTargetBtnStop);
+
+function onTargetBtnStart() {
+  timeoutId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+
+  btnStart.disabled = true;
+}
+function onTargetBtnStop() {
+  clearInterval(timeoutId);
+}

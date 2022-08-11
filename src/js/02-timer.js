d77fd15d7712c63b6d0acf771b0331f224cdcb84
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-08-11 19:32:42 +0300

    add 2 task

---
+++ src/js/02-timer.js
@@ -0,0 +1,71 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minEl = document.querySelector('[data-minutes]');
+const seconEl = document.querySelector('[data-seconds]');
+const startBtn = document.querySelector('[data-start]');
+let timerId = null;
+let setTime = 0;
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    const currentTime = new Date().getTime();
+    if (selectedDates[0] < currentTime) {
+      window.alert('Please choose a date in the future');
+      startBtn.disabled = true;
+      return;
+    } else {
+      setTime = selectedDates[0].getTime();
+      startBtn.disabled = false;
+    }
+  },
+};
+flatpickr('#datetime-picker', options);
+
+startBtn.addEventListener('click', onTargetBtn);
+function onTargetBtn() {
+  startBtn.disabled = true;
+  const timerId = setInterval(() => {
+    const timerTime = setTime - new Date().getTime();
+    if (timerTime <= 0) {
+      clearInterval(timerId);
+      return;
+    }
+
+    const obj = ({ days, hours, minutes, seconds } = convertMs(timerTime));
+
+    function addLeadingZero(value) {
+      return value.padStart(2, '0');
+    }
+    daysEl.textContent = addLeadingZero(String(days));
+    hoursEl.textContent = addLeadingZero(String(hours));
+    minEl.textContent = addLeadingZero(String(minutes));
+    seconEl.textContent = addLeadingZero(String(seconds));
+
+    // мені потрібно перебрати обьект щоб присвоїти значення потрібні в days,hours,minuts...
+  }, 1000);
+}

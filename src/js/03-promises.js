2d6c7ff56d18d206dc83c09123b1a3cc402487eb
Darina-Shvydiuk <darinakabakova@gmail.com>
2022-08-12 17:27:34 +0300

    last commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,6 +8,7 @@ function onSubmitForm(event) {
   for (let index = 0; index < form.amount.value; index += 1) {
     createPromise(
       index + 1,
+
       Number(form.delay.value) + Number(form.step.value) * index
     )
       .then(({ position, delay }) => {
@@ -35,41 +36,3 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-// import Notiflix from 'notiflix';
-// const formEl = document.querySelector('.form');
-
-// formEl.addEventListener('submit', onFormSubmit);
-
-// function onFormSubmit(evt) {
-//   evt.preventDefault();
-//   for (let index = 0; index < formEl.amount.value; index += 1) {
-//     createPromise(
-//       index + 1,
-//       Number(formEl.delay.value) + Number(formEl.step.value) * index
-//     )
-//       .then(({ position, delay }) => {
-//         Notiflix.Notify.success(
-//           `✅ Fulfilled promise ${position} in ${delay}ms`
-//         );
-//       })
-//       .catch(({ position, delay }) => {
-//         Notiflix.Notify.failure(
-//           `❌ Rejected promise ${position} in ${delay}ms`
-//         );
-//       });
-//   }
-// }
-
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const shouldResolve = Math.random() > 0.3;
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
